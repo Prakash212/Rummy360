aee02827558ad8bcd5bb5fbb0d127ddd83879243
Chinnabattini Bhanu Prakash <prakash.bhanu212@gmail.com>
2026-09-04 19:21:30 +0530

    Centralized application theme colors

--- Rummy360/AppShell.xaml.cs
+++ Rummy360/AppShell.xaml.cs
@@ -10,5 +10,13 @@ public partial class AppShell : Shell
         Routing.RegisterRoute(nameof(PlayersPage), typeof(PlayersPage));
         Routing.RegisterRoute(nameof(AddPlayerPage), typeof(AddPlayerPage));
         Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
+        Routing.RegisterRoute(nameof(NewGamePage), typeof(NewGamePage));
+        Routing.RegisterRoute(nameof(LiveScorePage), typeof(LiveScorePage));
+        Routing.RegisterRoute(nameof(RoundCompletedPage), typeof(RoundCompletedPage));
+        Routing.RegisterRoute(nameof(RoundEntryPage), typeof(RoundEntryPage));
+        Routing.RegisterRoute(nameof(RoundHistoryPage), typeof(RoundHistoryPage));
+        Routing.RegisterRoute(nameof(SeatingOrderPage), typeof(SeatingOrderPage));
+        Routing.RegisterRoute(nameof(SelectPlayersPage), typeof(SelectPlayersPage));
+        Routing.RegisterRoute(nameof(StatisticsPage), typeof(StatisticsPage));
     }
 }
--- Rummy360/Platforms/Android/MainActivity.cs
+++ Rummy360/Platforms/Android/MainActivity.cs
@@ -1,11 +1,48 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Graphics;
 using Android.OS;
+using Android.Views;
+using System.Runtime.Versioning;
 
 namespace Rummy360
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        [SupportedOSPlatform("android21.0")]
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            var window = Window;
+            if (window == null)
+                return;
+
+#pragma warning disable CA1416
+#pragma warning disable CS0618
+
+            // Brand Purple Status Bar
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+#pragma warning disable CA1422
+                window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#2E0A9E"));
+#pragma warning restore CA1422
+            }
+
+            // White icons on Android 11+ (API 30+)
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
+            {
+                window.InsetsController?.SetSystemBarsAppearance(
+                    0,
+                    (int)WindowInsetsControllerAppearance.LightStatusBars);
+            }
+            else
+            {
+                window.DecorView.SystemUiVisibility = 0;
+            }
+
+#pragma warning restore CS0618
+#pragma warning restore CA1416
+        }
     }
 }
